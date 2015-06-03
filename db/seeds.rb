9e0c019201a3b1d6861db99d2687009d7eee1c3e
scott m parrish <anithri@gmail.com>
2015-06-03 12:24:57 -0600

    refactoring a few views and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file .env file.
-
+require 'ffaker'
 player_account = PlayerAccount.create(confirmed_at: Date.today,
                                       email:        "#{ENV['PLAYER_ACCOUNT_SEED_EMAIL']}",
                                       password:     "#{ENV['PLAYER_ACCOUNT_SEED_PASSWORD']}")
-group          = GamingGroup.create(name:        'Example Group',
-                                    description: 'just a bunch of hoopy froods who like to game.',
+player_account.player.update(name: FFaker::Name.name)
+
+group = GamingGroup.create(name:        'Example Group',
+                           description: 'just a bunch of hoopy froods who like to game.',
 )
+
 group.group_members.create(player: player_account.player, role: :owner)
+
+10.times do
+  pa             = PlayerAccount.create(confirmed_at: Date.today,
+                                        email:        FFaker::Internet.email,
+                                        password:     FFaker::Internet.password)
+  pa.player.name = FFaker::Name.name
+  group.group_members.create(player: pa.player, role: GroupMember.roles.keys[1..-2].sample)
+
+end
