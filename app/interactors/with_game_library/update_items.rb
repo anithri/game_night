0127d773834186a3a0bb7d528f48493dbaf858f1
scott m parrish <anithri@gmail.com>
2015-07-09 17:31:50 -0600

    fixed messages for WithGameLibrary::UpdateItems

--- app/interactors/with_game_library/update_items.rb
+++ app/interactors/with_game_library/update_items.rb
@@ -7,16 +7,16 @@ class WithGameLibrary::UpdateItems
   def call
     prior_ids = game_summaries.pluck(:id)
     Array(game_summary_ids).map(&:to_i).each do |gsid|
-      gs = GameSummary.find(gsid.abs)
       if gsid > 0
         next if prior_ids.include?(gsid)
-        context.notice = "#{gs.name} added to library."
         game_summary = WithBggApi::FindSummarizeImportById.call(bgg_id: gsid, cache: context.cache).game_summary
         new_item = GameLibraryItem.find_or_create_by(game_library_id: game_library.id,
                                                      game_summary_id: gsid
         )
+        context.notice = "#{game_summary.name} added to library."
       else
         next unless prior_ids.include?(- gsid)
+        gs = GameSummary.find(gsid)
         context.notice = "#{gs.name} removed from library."
         GameLibraryItem.where(game_library: game_library,game_summary_id: - gsid).delete_all
       end
