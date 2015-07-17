840d73fb0760f32acac34d991e3abab2e44dedfd
scott m parrish <anithri@gmail.com>
2015-07-17 12:55:13 -0600

    Updated footer and linked documents

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,15 @@
 class VisitorsController < ApplicationController
+
+  def creative_commons
+    skip_authorization
+  end
+
+  def mit_license
+    skip_authorization
+  end
+
+  def privacy_policy
+    skip_authorization
+  end
+
 end
